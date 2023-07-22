67d83fae6b2ff34152b7a9b5d16e18cb87df44cf
sthsuyash <sthasuyash11@gmail.com>
2023-07-23 02:35:24 +0545

    javascript branching

--- src/client/src/components/PageHeader.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Heading } from 'evergreen-ui';
-
-export default function PageHeader(props) {
-    return (
-        <Heading size={900}>Welcome to Chat Us</Heading>
-    )
-};
