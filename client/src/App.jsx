19f94c486973dcbcd27260e3a3089da158e2929e
sthsuyash <sthasuyash11@gmail.com>
2023-07-23 02:38:06 +0545

    initial js commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='bg-red-500'>This is app</p>
+    </>
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx}',
+    './public/index.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
